2bd3cd218936912e9cd5145ee3524219f8c84942
Patycka <elektropatycka@gmail.com>
2021-08-09 14:53:19 +0200

    Create draw of database model. Added migrations. Added admin view.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Engineer(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    project = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.surname
+
+
+class PresenceInfo(models.Model):
+    date = models.DateTimeField('date ')
